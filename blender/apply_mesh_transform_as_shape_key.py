45a621e15b5297fab2b93a44b0f0dcf69b019067
BennyKok <itechbenny@gmail.com>
2023-09-04 17:29:13 +0800

    feat: add rotate, origin offset, transform radius and falloff props in blendshape node

--- blender/apply_mesh_transform_as_shape_key.py
+++ blender/apply_mesh_transform_as_shape_key.py
@@ -20,6 +20,14 @@ class ApplyMeshTransformAsShapeKey:
                 "scale_y": ("FLOAT", {"default": 1.0, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
                 "offset_x": ("FLOAT", {"default": 0.0, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
                 "offset_y": ("FLOAT", {"default": 0.0, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
+                
+                "rotate": ("FLOAT", {"default": 0, "min": -360, "max": 360.0, "step": 0.01, "display": "number"}),
+
+                "origin_offset_x": ("FLOAT", {"default": 0, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
+                "origin_offset_y": ("FLOAT", {"default": 0, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
+
+                "transform_radius": ("FLOAT", {"default": 1.0, "min": 0, "max": 1, "step": 0.01, "display": "number"}),
+                "falloff": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01, "display": "number"}),
             },
         }
 
@@ -28,12 +36,11 @@ class ApplyMeshTransformAsShapeKey:
 
     FUNCTION = "process"
 
-    # OUTPUT_NODE = False
-
     CATEGORY = "mesh"
 
-    def process(self, bpy_objs, shape_key_name, target_vertex_group, scale_x, scale_y, offset_x, offset_y):
-        from mathutils import Vector
+    def process(self, bpy_objs, shape_key_name, target_vertex_group, scale_x, scale_y, offset_x, offset_y, rotate, origin_offset_x, origin_offset_y, transform_radius, falloff):
+        from mathutils import Vector, Matrix
+        import math
         import global_bpy
         bpy = global_bpy.get_bpy()
 
@@ -44,19 +51,29 @@ class ApplyMeshTransformAsShapeKey:
         sk.interpolation = 'KEY_LINEAR'
 
         verts = obj.data.vertices
-        
+
         # get vertex group
         vertex_group = obj.vertex_groups[target_vertex_group]
 
         # get the verts in the group
-        verts_in_group = [v for v in verts if vertex_group.index in [vg.group for vg in v.groups]]
+        verts_in_group = [v for v in verts if vertex_group.index in [
+            vg.group for vg in v.groups]]
 
         # position each vert
         # for i in range(len(verts)):
         # Calculate center of mesh
         # center = sum((vert.co for vert in verts), Vector()) / len(verts)
-        center = sum((vert.co for vert in verts_in_group), Vector()) / len(verts_in_group)
-        # apply scale relative to object center 
+        center = sum((vert.co for vert in verts_in_group),
+                     Vector()) / len(verts_in_group)
+
+        # apply origin offset
+        center.x += origin_offset_x
+        center.y += origin_offset_y
+        # apply scale relative to object center
+
+        # Calculate max distance from center to any vertex in the group
+        max_distance = max(
+            (vert.co - center).length for vert in verts_in_group)
 
         # Position each vert
         for i, vert in enumerate(verts):
@@ -66,23 +83,46 @@ class ApplyMeshTransformAsShapeKey:
             # Get vector from center to vertex
             vec = vert.co - center
 
+            # Calculate distance from origin and weight factor
+            distance = vec.length
+            # weight = max(0, 1 - distance / (max_distance * transform_radius))
+
+            # if the vertex is outside the transform radius, skip it
+            if (distance / max_distance) > transform_radius:
+                continue
+
+            # calculate weight
+            weight = max(0, 1 - distance / (transform_radius * max_distance)) * falloff
+            weight = min(weight, 1)
+
+            # print(scale_x, scale_y, weight)
+
             # Scale vector
-            # if scale_x != 0:
-            vec.x *= scale_x
-            # if scale_x != 0:
-            vec.y *= scale_y
+            vec.x *= scale_x + (abs(scale_x - 1) * weight)
+            vec.y *= scale_y + (abs(scale_y - 1) * weight)
 
             # # Offset vector
-            vec.x += offset_x
-            vec.y += offset_y
+            vec.x += offset_x * weight
+            vec.y += offset_y * weight
+
+            # Rotate vector
+            # rotate_right_rad = math.radians(rotate_right)
+            rotate_rad = math.radians(rotate)
+
+            # Create rotation matrices for the right and left rotations
+            # rotation_matrix_right = Matrix.Rotation(rotate_right_rad, 4, 'X')
+            rotation_matrix = Matrix.Rotation(rotate_rad, 4, 'Z')
+
+            # Apply the rotation matrices to the vector
+            # vec.rotate(rotation_matrix_right)
+            vec.rotate(rotation_matrix)
 
             # Calculate new position
             new_pos = center + vec
 
             # Apply new position
-            # vert.co = new_pos
             sk.data[i].co = new_pos
-            
+
         return (bpy_objs, )
 
 
