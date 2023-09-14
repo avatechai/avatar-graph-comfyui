f86081ab8d5f0c2dd68d876f042e2fa9dccdd5a8
BennyKok <itechbenny@gmail.com>
2023-09-14 18:34:58 +0800

    feat: add get image wh node

---
+++ blender/image_width_height.py
@@ -0,0 +1,20 @@
+import blender_node
+
+
+class GetImageWidthHeight(blender_node.ObjectOps):
+    BASE_INPUT_TYPES = {
+        "image": ('IMAGE',),
+        "scale": ('FLOAT', {'default': 1.0,})
+    }
+
+    RETURN_TYPES = ('FLOAT', 'FLOAT',)
+    RETURN_NAMES = ('width', 'height',)
+
+    def blender_process(self, bpy, image, scale, **props):
+
+        height, width  = image[0].shape[:2]
+
+        print(height, width)
+        print((width * scale, height * scale, ))
+
+        return (width * scale, height * scale, )
