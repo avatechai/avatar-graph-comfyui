248af1ef9b3c805633ad3960c959a05a69f08ccb
BennyKok <itechbenny@gmail.com>
2023-09-14 19:45:19 +0800

    fix: update avatar view

--- js/AvatarViewer.js
+++ js/AvatarViewer.js
@@ -1,17 +1,16 @@
-import { iframeSrc, showViewer } from './state.js'
 import { van } from './van.js'
 const { button, iframe, div, img } = van.tags
 
 export function AvatarViewer() {
   return div(
     {
-      class: 'w-full h-full max-w-96 max-h-96 absolute right-0 top-0 z-[100] pointer-events-auto',
+      class: 'w-[310px] h-[310px] absolute right-0 top-0 z-[100] pointer-events-auto mt-4 mr-4',
     },
     iframe({
       id: 'avatech-viewer-iframe',
       title: 'avatech-viewer-iframe',
       name: 'avatech-viewer-iframe',
-      class: () => 'w-full h-full flex pointer-events-auto ',
+      class: () => 'w-full h-full flex pointer-events-auto rounded-2xl border-none',
       src: 'https://labs.avatech.ai/viewer/default',
       // src: 'http://localhost:3000/viewer/default',
     })
