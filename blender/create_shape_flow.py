93d1d8f0a67d711489e69f83115c4b1d1e7dd011
Edwin Wong <ew20128@gmail.com>
2023-09-26 13:22:39 +0800

    fix: naming

--- blender/create_shape_flow.py
+++ blender/create_shape_flow.py
@@ -1,18 +1,18 @@
 
-class CreateShapesFlow():
+class CreateShapeFlow():
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "blendshapes": ("STRING", {
+                "shape_flow": ("STRING", {
                     "multiline": False,
                     "default": "{node: ''}"
                 }),
             },
         }
 
-    RETURN_TYPES = ("blendshapes",)
-    RETURN_NAMES = ("blendshapes",)
+    RETURN_TYPES = ("shape_flow",)
+    RETURN_NAMES = ("shape_flow",)
     FUNCTION = "process"
 
     CATEGORY = "mesh"
@@ -23,10 +23,10 @@ class CreateShapesFlow():
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "CreateShapesFlow": CreateShapesFlow
+    "CreateShapeFlow": CreateShapeFlow
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CreateShapesFlow": "CreateShapesFlow"
+    "CreateShapeFlow": "CreateShapeFlow"
 }
