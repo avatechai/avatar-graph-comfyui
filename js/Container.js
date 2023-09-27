85b2de630384be3bf030b3106eabd42fc17092ff
BennyKok <itechbenny@gmail.com>
2023-09-27 17:45:32 +0800

    fix styling

--- js/Container.js
+++ js/Container.js
@@ -9,7 +9,7 @@ const { button, iframe, div, img } = van.tags;
 export function Container() {
   return div(
     {
-      class: 'fixed left-0 top-0 w-full h-full z-[200] pointer-events-none',
+      class: 'fixed left-0 top-0 w-full h-full z-[1000] pointer-events-none',
       id: 'avatech-editor',
     },
     ShapeFlowEditor(),
