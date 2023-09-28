817b982f4e57125b39652b1cdd3740341e9efd47
Radionic <khpluslt@gmail.com>
2023-09-28 18:48:52 +0800

    fix: occasional segmentation fault

--- blender/ops_join.py
+++ blender/ops_join.py
@@ -9,13 +9,11 @@ class Mesh_JoinMesh(blender_node.ObjectOps):
     CUSTOM_NAME = "Join Meshes"
 
     def blender_process(self, bpy, BPY_OBJ, **props):
-        
-        prop_values = props.values()
-        
-        override = bpy.context.copy()
-        override["active_object"] = BPY_OBJ
-        override["selected_editable_objects"] = list(prop_values) + [BPY_OBJ]
-        bpy.ops.object.join(override)
+        prop_values = props.values()        
+        for obj in list(prop_values) + [BPY_OBJ]:
+            obj.select_set(True)
+        bpy.context.view_layer.objects.active = BPY_OBJ
+        bpy.ops.object.join()
 
         return (BPY_OBJ,)
 
